5d167f2fe15e2eb531ecc2aca60ea76fade35745
radenska <yana_radenska@yahoo.com>
2017-01-03 11:03:00 -0800

    committing changes prior to mere

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 'use strict'
 
-function proj(name, path, image, description) {
+var projArray = [];
+
+function Proj(name, path, image, description) {
   this.name = name;
   this.path = path;
   this.img = image;
   this.description = description;
 }
+
+projArray[0] = new Proj('The Cookie Stand public site', 'https://radenska.github.io/cookie-stand', '', 'The Cookie Stand public site is the customer site for a cookie store franchise. Users can learn more about the franchise, including locations, hours, and merchandise. They can also buy merchanise through an online order form.');
+projArray[1] = new Proj('About Me', 'https://radenska.github.io/about_me', '', 'The about me page quizzes the user on a few aspects of my (Yana\'s) life before allowing them to see a brief site about me as well as displaying how they did on the questions.');
+projArray[2] = new Proj('Bus Mall', 'https://radenska.github.io/bus-mall', '', 'This site is made for a product research team polling user choices in order to decide what to include in a merchandise catalog to be available on commuter buses.');
+
+console.log(projArray);
 
 function displayProj() {
   //HAVE TO FIGURE OUT THE TABLE APPENDING - IT NO WORKEE RIGHT NOW, BUT THE TEST DOES
