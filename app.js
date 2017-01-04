775a8ef6dbb8765f44535d2b512fbade5aa1d1a7
radenska <yana_radenska@yahoo.com>
2017-01-04 08:00:00 -0800

    made separate js file for project info, made toHtml prototype function, create base, layout, and module css files

--- app.js
+++ app.js
@@ -5,28 +5,45 @@ var projArray = [];
 function Proj(name, path, image, description) {
   this.name = name;
   this.path = path;
-  this.img = image;
   this.description = description;
+  this.img = image;
 }
 
-projArray[0] = new Proj('The Cookie Stand public site', 'https://radenska.github.io/cookie-stand', '', 'The Cookie Stand public site is the customer site for a cookie store franchise. Users can learn more about the franchise, including locations, hours, and merchandise. They can also buy merchanise through an online order form.');
-projArray[1] = new Proj('About Me', 'https://radenska.github.io/about_me', '', 'The about me page quizzes the user on a few aspects of my (Yana\'s) life before allowing them to see a brief site about me as well as displaying how they did on the questions.');
-projArray[2] = new Proj('Bus Mall', 'https://radenska.github.io/bus-mall', '', 'This site is made for a product research team polling user choices in order to decide what to include in a merchandise catalog to be available on commuter buses.');
+projInfo.prototype.toHtml = function() {
+  var $newProj = $('section.invisible').clone();
+  $newProj.find('h3 a').text(this.name)
+  $newProj.find('h3 a').attr('href', this.path);
+  $newProj.find('p').text('this.description');
+  /* TODO: We also need to fill in:
+  1. author name
+  2. author url
+  3. article title
+  4. article body
+  5. publication*/
+  $newProj.removeClass('invisible');
+  return $newProj;
+};
 
-function displayProj() {
-  //HAVE TO FIGURE OUT THE TABLE APPENDING - IT NO WORKEE RIGHT NOW, BUT THE TEST DOES
-  $(document).ready(function() {
-    var tbl = '<table>';
-    for (var i = 0; i < projArray.length; i ++) {
-      tbl += '<tr>';
-      tbl += '<td>' + projArray[i].name + '</td>';
-      tbl += '<td>' + projArray[i].path + '</td>';
-      tbl += '<td>' + projArray[i].img + '</td>';
-      tbl += '<td>' + projArray[i].description + '</td>';
-    }
-    tbl += '</table>';
-    $(tbl).appendTo('section');
-  });
-}  //how do i call the function here immediately?
+projInfo.forEach(function(value) {
+  projArray.push(new Proj(value));
+});
 
-displayProj();
+projInfo.forEach(function(project) {
+  $('section').append(project.toHtml());
+});
+
+// function displayProj() {
+//   $(document).ready(function() {
+//     var tbl = '<table>';
+//     for (var i = 0; i < projArray.length; i ++) {
+//       tbl += '<tr>';
+//       tbl += '<td>' + projArray[i].name + '</td>';
+//       tbl += '<td>' + projArray[i].path + '</td>';
+//       tbl += '<td>' + projArray[i].img + '</td>';
+//       tbl += '<td>' + projArray[i].description + '</td>';
+//     }
+//     tbl += '</table>';
+//     $(tbl).appendTo('section');
+//   });
+// }  //how do i call the function here immediately?
+// displayProj();
