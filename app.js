1e24f9e0b9d6f3539eb3798237d7466f328d5850
radenska <yana_radenska@yahoo.com>
2017-01-05 16:19:54 -0800

    replaced template with handlebars template

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 'use strict';
 
-var projArray = [];
+var projArray = []; //stores project objects
+var navHandle = {}; //object with methods that handles clicking on the nav element
 
-function Proj(individualProject) { //constructor
-  this.name = individualProject.name;
-  this.path = individualProject.path;
-  this.description = individualProject.description;
-  this.img = individualProject.img;
+function Proj (obj) {
+  for(var key in obj)
+    this[key] = obj[key];
 }
 
+function Proj
+
 Proj.prototype.toHtml = function() { //does the actual appending to the html
-  var $newProj = $('section.visible').clone();
-  $newProj.find('h3 a').text(this.name)
-  $newProj.find('h3 a').attr('href', this.path);
-  $newProj.find('p').text(this.description);
-  $newProj.removeClass('visible');
-  return $newProj;
+  var source = $('#projects-template').html();
+  var renderArticle = Handlebars.compile(source);
+  var context = articles;
+  return renderArticle(this);
 };
 
 projectInfo.forEach(function(ele) { //take the variable from projInfo.js and use the constructor to make each array element an object, which gets pushed to an array
@@ -26,22 +25,31 @@ projArray.forEach(function(project) { //append each Proj object to the html
   $('#projects').append(project.toHtml());
 });
 
-function handleNavClick () {
+navHandle.prototype.handleNavClick (function() {
   $('.tab').on('click', function() {
     $('article').hide();
     var tempDataName = $(this).data('name');
     $('#' + tempDataName).fadeIn();
+    if ($('.hamburger').css('display') === 'block') {
+      $('nav ul').css('display', 'none');
+    }
   });
   $('.tab:first').click();
-}
+});
 
-function handleHamburgerClick () {
-  $('nav ul').on('click', function() {
-    // var tempDisplay = $(this).find().css('display');
-    // console.log(tempDisplay);
-    $(this).addClass('menu');
+navHandle.prototype.handleHamburgerClick (function() {
+  $('.hamburger').on('click', function() {
+    if ($('.hamburger').css('display') === 'block') {
+      var displayVal = $('nav ul').css('display');
+      if (displayVal === 'none') {
+        $('nav ul').css('display','block');
+      }
+      else {
+        $('nav ul').css('display','none');
+      }
+    }
   });
-}
-
-handleNavClick();
-handleHamburgerClick();
+});
+//these two functions handle clicking on the nav menu; in mobile mode, this includes making the ul (tabs) visible on a click as well as invisible if the person either clicks a tab or link OR they click the hamburger again; in either view, they also display and hide the appropriate content based on user click; the default is "about"
+navHandle.handleNavClick();
+navHandle.handleHamburgerClick();
