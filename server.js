8527d79d158fba5252e4d5ce56990401735ac357
radenska <yana_radenska@yahoo.com>
2017-01-10 17:57:53 -0800

    node server up and running per instructions

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4242;
+
+app.use(express.static('./public'));
+
+app.get('index.html', function(request, response) {
+  response.sendFile('index.html', {root: './public'});
+});
+
+app.get('*', function(request, response) {
+  response.status(404).sendFile('404.html', {root: './public'});
+});
+
+app.listen(PORT, function() {
+  console.log('Server is up and running on port ' + PORT + ' and can be accessed at localhost:' + PORT);
+});
