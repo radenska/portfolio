c2e06f119e0b0b2e6e6d9ee68c74b3fc339f8397
radenska <yana_radenska@yahoo.com>
2017-01-15 15:06:55 -0800

    streamlined code, chained where feasible, reduced use of variables, changed variables to let where possible, wrapped both js files in anonymous function, used map array method

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,78 +1,69 @@
 'use strict';
 
-var navHandle = {}; //object with methods that handles clicking on the nav element
-var proj = {};
+(function(appModule) {
+  var proj = {};
+  appModule.proj = proj;
 
-proj.getProjInfo = function() {
-  $.getJSON('/data/projInfo.json') //this replaced the for ... in loop used to get the info out of the projInfo.js file
-    .then(function successCallback(projInfo) {
-      localStorage.projInfo = JSON.stringify(projInfo);
-      location.reload();
-    }, function failCallback() {
-      console.log('oops, you have failed to get the data from the JSON file!');
-    });
-}
-
-proj.handleETag = function() {
-  var reqETag = $.ajax({
-    type: 'HEAD',
-    url: '/data/projInfo.json',
-    dataType: 'json',
-    success: function successCallback() {
-      var headers = (reqETag.getAllResponseHeaders()).split('\n');
-      var ETag;
-      headers.forEach(function(val) {
-        if (val.slice(0,4) === 'ETag') {
-          ETag = JSON.stringify(val);
-        }
+  proj.getProjInfo = function() {
+    $.getJSON('/data/projInfo.json') //this replaced the for ... in loop used to get the info out of the projInfo.js file
+      .then(function (projInfo) {
+        localStorage.projInfo = JSON.stringify(projInfo);
+        location.reload();
+      }, function () {
+        console.log('oops, you have failed to get the data from the JSON file!');
       });
-      if (!localStorage.ETag || localStorage.ETag !== ETag) {
-        localStorage.ETag = ETag;
-        proj.getProjInfo();
+  }
+
+  proj.handleETag = function() {
+    var ETag;
+    let reqETag = $.ajax({
+      type: 'HEAD',
+      url: '/data/projInfo.json',
+      dataType: 'json',
+      success: function () {
+        (reqETag.getAllResponseHeaders()).split('\n').forEach(function(val) {
+          if (val.slice(0,4) === 'ETag') ETag = JSON.stringify(val);
+        });
+        if (!localStorage.ETag || localStorage.ETag !== ETag) {
+          localStorage.ETag = ETag;
+          proj.getProjInfo();
+        }
       }
-    }
-  }).fail('your HEAD request failed.');
-}
+    }).fail('your HEAD request failed.');
+  }
 
-proj.renderEach = function() {
-  console.log('in renderEach function');
-  var projInfo = JSON.parse(localStorage.projInfo);
-  projInfo.forEach(function(project) { //append each Proj object to the html
-    var source = $('#projects-template').html();
-    var renderArticle = Handlebars.compile(source);
-    $('#projects').append(renderArticle(project));
-  });
-}
+  proj.renderEach = function() {
+    JSON.parse(localStorage.projInfo).map(function(project) { //append each Proj object to the html
+      let source = $('#projects-template').html();
+      let renderArticle = Handlebars.compile(source);
+      $('#projects').append(renderArticle(project));
+    });
+  }
 
-navHandle.handleNavClick = function () {
-  $('.tab').on('click', function() {
-    $('article').hide();
-    var tempDataName = $(this).data('name');
-    $('#' + tempDataName).fadeIn();
-    if ($('.hamburger').css('display') === 'block') {
-      $('nav ul').css('display', 'none');
-    }
-  });
-  $('.tab:first').click();
-}
-
-navHandle.handleHamburgerClick = function() {
-  $('.hamburger').on('click', function() {
-    if ($('.hamburger').css('display') === 'block') {
-      var displayVal = $('nav ul').css('display');
-      if (displayVal === 'none') {
-        $('nav ul').css('display','block');
+  proj.handleNavClick = function () {
+    $('.tab').on('click', function() {
+      $('article').hide();
+      $('#' + $(this).data('name')).fadeIn();
+      if ($('.hamburger').css('display') === 'block') {
+        $('nav ul').css('display', 'none');
       }
-      else {
-        $('nav ul').css('display','none');
-      }
-    }
-  });
-}
+    });
+    $('.tab:first').click();
+  }
 
-//these two functions handle clicking on the nav menu; in mobile mode, this includes making the ul (tabs) visible on a click as well as invisible if the person either clicks a tab or link OR they click the hamburger again; in either view, they also display and hide the appropriate content based on user click; the default is "about"
-$(document).ready(function() {
-  proj.handleETag();
-  navHandle.handleNavClick();
-  navHandle.handleHamburgerClick();
-});
+  proj.handleHamburgerClick = function() {
+    $('.hamburger').on('click', function() {
+      if ($('.hamburger').css('display') === 'block') {
+        if ($('nav ul').css('display') === 'none') $('nav ul').css('display','block');
+        else $('nav ul').css('display','none');
+      }
+    });
+  }
+
+  $(document).ready(function() {
+    proj.handleETag();
+    proj.handleNavClick();
+    proj.handleHamburgerClick();
+  });
+
+})(window);
