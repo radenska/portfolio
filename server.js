3396e27415d51044ea0115757472d76335455622
radenska <yana_radenska@yahoo.com>
2017-01-19 17:05:43 -0800

    made changes to structure to hide token better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // const pg = require('pg');
 const express = require('express');
 const app = express();
+const requestProxy = require('express-request-proxy');
 
 const PORT = process.env.PORT || 4242;
 // const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
@@ -16,6 +17,14 @@ app.get('index.html', function(request, response) {
 app.get('/about', function(request, response) {
   response.sendFile('index.html', {root: './public'});
 })
+
+app.get('/github/*', function proxyGitHub(request, response) {
+  console.log('Routing a GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+});
 
 app.get('*', function(request, response) {
   response.status(404).sendFile('404.html', {root: './public'});
